93aa55c1b4db54832d175ea026392fca1df693c1
OmerHalil <gamega55834@gmail.com>
2022-09-09 12:24:44 +0300

    Theme Change added with Context api

---
+++ src/context/Theme/ThemeContext.jsx
@@ -0,0 +1,25 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import { createContext, useMemo, useState } from "react";
+import { darkTheme, lightTheme } from "../../screens/main/themes";
+
+export const ThemeContext = createContext({
+  mode: "light",
+  toggleTheme: () => {},
+});
+
+export const ThemeModeProvider = ({ children }) => {
+  const [mode, setMode] = useState("light");
+
+  const toggleTheme = () => setMode(mode === "light" ? "dark" : "light");
+
+  const theme = useMemo(
+    () => createTheme(mode === "light" ? lightTheme : darkTheme),
+    [mode]
+  );
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
---
+++ src/screens/main/themes.jsx
@@ -0,0 +1,15 @@
+import { lightBlue, orange, red } from "@mui/material/colors";
+
+export const lightTheme = {
+  palette: {
+    mode: "light",
+    primary: red,
+  },
+};
+
+export const darkTheme = {
+  palette: {
+    mode: "dark",
+    primary: orange,
+  },
+};
