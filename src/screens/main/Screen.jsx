d42bb3d14b0d783b353857bd4625981f97ad563e
OmerHalil <gamega55834@gmail.com>
2022-09-24 15:00:29 +0300

    adding nested routes.

--- src/screens/main/Screen.jsx
+++ src/screens/main/Screen.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import { Button, createTheme, CssBaseline, Stack } from "@mui/material";
 import { ThemeModeProvider } from "../../context/Theme/ThemeContext";
 import Header from "components/Header";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router-dom";
 
 const Main = () => {
   return (
     <ThemeModeProvider>
       <CssBaseline />
-      <Header />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>Layout</div>} />
-          <Route path="home" element={<Home />} />
-          <Route path="contact" element={<div>Contact</div>} />
-          <Route path="*" element={<div>No Page</div>} />
+          <Route path="/" element={<App />}>
+            <Route path="home" element={<Home />} />
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </ThemeModeProvider>
@@ -33,6 +32,15 @@ const Home = () => {
         <Link to="/home">Home</Link> | <Link to="/contact">Contact</Link>
       </nav>
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
   );
 };
 
