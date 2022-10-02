1cc2c80964a527cf6762908aa7e54f092a4dbce6
OmerHalil <gamega55834@gmail.com>
2022-10-02 18:00:16 +0300

    Index page changed.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
+import App from "App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Main from "./screens/main/Screen";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>
 );
 
