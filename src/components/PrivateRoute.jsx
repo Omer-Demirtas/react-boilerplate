61747e4ba35076966896d579aac38742b1bc9fcf
Omer-Demirtas <gamega55834@gmail.com>
2022-10-24 22:38:48 +0300

    Delete unnecessary code.

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -4,9 +4,6 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children, roles }) => {
   const { user } = useAuth();
 
-  console.log({ user, roles });
-  console.log(roles.filter((element) => user.roles.includes(element)));
-
   if (roles && !roles.filter((element) => user.roles.includes(element)).length )
     return <Navigate to="/notFound" />;
   if (!user) return <Navigate to="/auth/login" />;
