d58076696b411d5ce9d610f4c4e262b0de598edd
Omer-Demirtas <gamega55834@gmail.com>
2022-10-30 12:19:01 +0300

    Login feature added to system

--- src/context/Auth/AuthContext.jsx
+++ src/context/Auth/AuthContext.jsx
@@ -3,6 +3,7 @@ import { useState, createContext, useContext, useEffect } from "react";
 const Context = createContext({ user: false, setUser: () => {} });
 
 export const AuthProvider = ({ children }) => {
+  const [isLogin, setIsLogin] = useState(false);
   const [user, setUser] = useState(
     JSON.parse(localStorage.getItem("user")) || false
   );
@@ -10,11 +11,20 @@ export const AuthProvider = ({ children }) => {
   const logout = () => setUser(false);
 
   useEffect(() => {
-    localStorage.setItem("user", JSON.stringify(user));
+    if (user) {
+      setIsLogin(true);
+      //localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      fetch("https://jsonplaceholder.typicode.com/users/1")
+        .then((response) => response.json())
+        .then((json) => setUser(json));
+    }
   }, [user]);
 
   return (
-    <Context.Provider value={{ user, setUser, logout }}>{children}</Context.Provider>
+    <Context.Provider value={{ user, isLogin, setUser, logout }}>
+      {children}
+    </Context.Provider>
   );
 };
 
