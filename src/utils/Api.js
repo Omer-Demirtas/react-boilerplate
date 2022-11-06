9df9c7113e9f8840646aba50d2067ea88280659a
Omer-Demirtas <gamega55834@gmail.com>
2022-11-06 17:50:37 +0300

    Api headers added

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const token = "ASDFGHJKL>ZXCVBNMÖ";
+
 export default axios.create({
-  baseURL: `https://jsonplaceholder.typicode.com/`
+  baseURL: `https://jsonplaceholder.typicode.com/`,
+  headers: {'Authorization': `Bearer ${token}`}
 });
