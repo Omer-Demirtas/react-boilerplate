069bed10782ff7bd94b0b92876bc9296900a68d9
Omer-Demirtas <gamega55834@gmail.com>
2022-12-10 21:55:56 +0300

    Remove Home leements

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,5 @@
-import { Button } from "@mui/material";
 import { useState } from "react";
 import { useUserStore } from "stores/UserStore";
-import HookSamples from "./components/HookSamples";
-import HookSample2 from "./components/HooksSample2";
 
 const Home = () => {
   const [state, setState] = useState(1);
@@ -12,12 +9,7 @@ const Home = () => {
 
   return (
     <div style={{ textAlign: "center" }}>
-      <div>
-        <h3>{user.username || "Not Found!"}</h3>
-        {/* <HookSamples initCount={state} handleIncrease={handleIncrease} /> */}
-        {/* <HookSample2 /> */}
-        <button onClick={() => setUser("Ömer")}>Set user</button>
-      </div>
+        <h1>Wellcome!</h1>
     </div>
   );
 };
